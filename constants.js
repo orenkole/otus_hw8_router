cd5482a58e367d86352b1914d463caa4ad983127
badger <badger@badger.local>
2021-12-01 13:54:20 +0200

    added build directory

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-const {resolve} = require('path');
-const {path: PROJECT_ROOT} = require('app-root-path');
+const { resolve } = require("path");
+const { path: PROJECT_ROOT } = require("app-root-path");
 
 exports.PROJECT_ROOT = PROJECT_ROOT;
-exports.BUILD_DIRECTORY = resolve(PROJECT_ROOT, './dist');
-exports.SOURCE_DIRECTORY = resolve(PROJECT_ROOT, './src');
+exports.BUILD_DIRECTORY = resolve(PROJECT_ROOT, "./build");
+exports.SOURCE_DIRECTORY = resolve(PROJECT_ROOT, "./src");
