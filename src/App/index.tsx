641e8a995abfee1d0ad3b2c0275440325535988b
badger <badger@badger.local>
2022-01-17 07:58:53 +0200

    test autoformat

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,56 +1,46 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 import { Field } from "@/components/Field";
 import { ControlPanel } from "@/components/ControlPanel";
-import {
-  AppStateType,
-} from "@/common/types";
+import { AppStateType } from "@/common/types";
 import { reducer } from "@/common/reducer";
 import { setInitialFieldInfo } from "@/common/helperFunctions";
 import { Container } from "@/elements/Container";
 import { Box } from "@/elements/Box";
 import { appStyles } from "./style";
-import { SignInForm } from "@/components/SignInForm";
+import {SignInForm} from "@/components/SignInForm";
 
-const initialFieldParams = {width: 5, height: 5, fillingPercentage: 0};
+const initialFieldParams = { width: 5, height: 5, fillingPercentage: 0 };
 
 export const initialState: AppStateType = {
   fieldInfo: setInitialFieldInfo({
     width: initialFieldParams.width,
     height: initialFieldParams.height,
-    fillingPercentage: initialFieldParams.fillingPercentage
+    fillingPercentage: initialFieldParams.fillingPercentage,
   }),
   fillingPercentage: initialFieldParams.fillingPercentage,
   width: initialFieldParams.width,
-  height: initialFieldParams.height
+  height: initialFieldParams.height,
 };
 
 const App = () => {
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div css={appStyles}>
       <Container>
-        <Box
-          display="grid"
-          justifyItems="center"
-          rowGap="50px"
-        >
+        <Box display="grid" justifyItems="center" rowGap="50px">
           <SignInForm />
-          <ControlPanel 
+          <ControlPanel
             dispatch={dispatch}
             fillingPercentage={state.fillingPercentage}
             width={state.width}
             height={state.height}
           />
-          <Field
-            fieldInfo={state.fieldInfo}
-            dispatch={dispatch}
-          />
+          <Field fieldInfo={state.fieldInfo} dispatch={dispatch} />
         </Box>
       </Container>
     </div>
   );
 };
 
-export {App};
+export { App };
